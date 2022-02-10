cbe2739fcae2be1524d48081f28561281151f94f
Tharindu Kumarasinghe <57245404+TharinduK97@users.noreply.github.com>
2022-02-10 16:48:38 +0530

    Develop: Role feature completes

---
+++ Controllers/RoleController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Smart_Cookers.Dtos.RoleDtos;
+using Smart_Cookers.Models;
+using Smart_Cookers.Services.RoleService;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Smart_Cookers.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RoleController : ControllerBase
+    {
+        private readonly IRoleService _roleservice;
+
+        public RoleController(IRoleService roleservice)
+        {
+            _roleservice = roleservice;
+
+        }
+
+        [HttpGet("GetAll")]
+        public async Task<ActionResult<ServiceResponse<List<GetRoleDto>>>> Get()
+        {
+            return Ok(await _roleservice.GetAllRoles());
+        }
+       
+
+        [HttpPost]
+        public async Task<ActionResult<ServiceResponse<List<GetRoleDto>>>> AddJob(AddRoleDto newrole)
+        {
+            return Ok(await _roleservice.AddRole(newrole));
+        }
+
+
+
+
+    }
+}
---
+++ Services/RoleService/IRoleService.cs
@@ -0,0 +1,13 @@
+﻿using Smart_Cookers.Dtos.RoleDtos;
+using Smart_Cookers.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Smart_Cookers.Services.RoleService
+{
+    public interface IRoleService
+    {
+        Task<ServiceResponse<List<GetRoleDto>>> GetAllRoles();
+        Task<ServiceResponse<List<GetRoleDto>>> AddRole(AddRoleDto newRole);
+    }
+}
